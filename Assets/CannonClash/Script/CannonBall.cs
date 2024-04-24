1fc1bad0d3c664cf793d1b8fc6341f176bfe3e47
Zhanbo <13489103779@163.com>
2024-04-24 14:04:58 -0400

    Polish Grabbing

---
+++ Assets/CannonClash/Script/CannonBall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CannonBall : MonoBehaviour
+{
+    // private XRGrabInteractable _grabInteractable;
+
+    public  bool catchable = false; // the b
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    
+}
